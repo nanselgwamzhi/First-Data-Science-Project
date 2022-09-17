a25259d94e792ec778f3e8766ca6b9e160a53020
nanselgwamzhi <113234051+nanselgwamzhi@users.noreply.github.com>
2022-09-17 22:53:46 +0100

    My firsr Jupyter Notebook on Watson Studion
    It is part of the peer-review assignment of the course tools for data as one of the courses of IBM Data Science Professional Certificate on Coursera.

---
+++ My first Jupyter Notebook on Watson Studio.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on Watson Studio #
+
+# **Gwamzh Nansel Dogara**<br>
+# Data Scientist
+
+# *I am interested in data science because it is the sexiest job of the 21st century*
+
+# ### This simple code displays the sentence "Data science is the sexiest job of the 21st century" ###
+
+# In[1]:
+
+
+print("Data science is the sexiest job of the 21st century")
+
+
+# In[ ]:
+
+
+> Joseph Campbell said: "If you follow your bliss, the universe will open doors for you where there were only walls"
+
+This why I have decided to switch careers to data science because with it, I can take up any of the following roles
+
+1. Data Scientist
+2. Data Engineer
+3. Business Analyst
+4. Machine Learning Engineer
+5. Data Researcher
+
+| Programming Language skill | Nontechnical skill | Priority |
+| --- | --- | --- |
+| Python | curiosity | Very Important|
+| SQL   | Judgemental | Very Important| 
+
+
+
+# In[ ]:
+
+
+
+
